535db2f800e33857d8dfccb7d6ce64f52ded450f
vojtmi <117769922+vojtmi@users.noreply.github.com>
2025-04-01 09:50:07 +0200

    add comments

--- shelly.js
+++ shelly.js
@@ -1,4 +1,5 @@
 const result = Shelly.getComponentStatus("Switch:0");
+//Set value from analog in [WATT] 
 var complet = 16611 + result.aenergy.total;
-
+//Print to console final value
 console.log("Celkova hodnota: " +complet/1000 + " kWh")
